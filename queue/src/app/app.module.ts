e43d73f5310b175e5d40d82bce6f8e9b27d6077e
ihor <igorlugovyi@gmail.com>
2018-06-05 08:46:15 +0300

    Add template for autocomplete

--- queue/src/app/app.module.ts
+++ queue/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
@@ -7,6 +9,7 @@ import { AppRoutingModule } from './/app-routing.module';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 import { LoginComponent } from './login/login.component';
 import { QueueComponent } from './queue/queue.component';
+
 
 @NgModule({
   declarations: [
@@ -17,6 +20,8 @@ import { QueueComponent } from './queue/queue.component';
     QueueComponent
   ],
   imports: [
+    NgbModule.forRoot(),
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
